6491d6f49b0a86e17eba299a0bf90eddcc288c3b
hafsaidiousse <hafsaidiousse@gmail.com>
2023-03-17 14:37:56 -0700

    commit 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+
+	while (i < 26)
+	{
+		putchar(alphabet[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
