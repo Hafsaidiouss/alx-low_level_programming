c67d5f9a354cf3e4efbf527d19ec479a990877de
hafsaidiousse <hafsaidiousse@gmail.com>
2023-03-18 06:45:17 -0700

    commit 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					if (i <= k && j < l)
+					{
+						putchar('0' + i);
+						putchar('0' + j);
+						putchar(' ');
+						putchar('0' + k);
+						putchar('0' + l);
+
+					
+						if (i != 9 || j != 8 || k != 9 || l != 9)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
