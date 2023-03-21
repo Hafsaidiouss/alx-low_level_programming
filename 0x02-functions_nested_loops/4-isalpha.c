d39f713c93e39ce9cc372e7fd35ead33a9754497
hafsaidiousse <hafsaidiousse@gmail.com>
2023-03-21 14:44:37 -0700

    commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * @c : character to check
+ * Return : 1 is alpha 0 if not
+ */
+int _isalpha(int c)
+{
+	int k;
+
+	if (c <= 122 && c >= 67)
+	{
+		k = 1;
+	}
+	else if (c <= 90 && c >= 65)
+	{
+		k = 1;
+	}
+	else
+	{
+		k = 0;
+	}
+	return (k);
+}
