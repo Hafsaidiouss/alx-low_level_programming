b5cecf2987ef3350ef463059bbbd124dd4721ebf
hafsaidiousse <hafsaidiousse@gmail.com>
2023-03-21 15:23:30 -0700

    commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @a : a number
+ * Return: the last digit
+ */
+int print_last_digit(int a)
+{
+	int k;
+
+	k = a % 10;
+	_putchar('0' + k);
+	return (k);
+}
