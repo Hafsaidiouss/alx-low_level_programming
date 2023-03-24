77740212e66121a929d2db2d5468ce5c5e526dff
hafsaidiousse <hafsaidiousse@gmail.com>
2023-03-23 18:18:29 -0700

    commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,17 +9,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+                {
+                        printf("FizzBuzz");
+                }
+		else if (i % 3 == 0)
 		{
 			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
-		}
-		else if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz");
 		}
 		else
 		{
@@ -28,6 +28,7 @@ int main(void)
 		if (i < 100)
 		{
 			printf(" ");
+
 		}
 	}
 	printf("\n");
