4b4b8068670052174524bc33a23ddceaed14abee
hafsaidiousse <hafsaidiousse@gmail.com>
2023-03-27 17:42:28 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts_half - rints half of a string,
+ * @str : string
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int j;
+
+	while (*(str + i) != 0)
+	{
+		i++;
+	}
+	if (i % 2 == 0)
+	{
+		for (j = i / 2; j < i; j++)
+		{
+			_putchar(*(str + j));
+		}
+	}
+	else
+	{
+		for (j = (i - 1) / 2; j < i; j++)
+		{
+			_putchar(*(str + j));
+		}
+	}
+	_putchar('\n');
+}
