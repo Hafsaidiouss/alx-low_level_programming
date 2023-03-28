8d6c78233586b416d60f99079c5b9ed662148f3b
hafsaidiousse <hafsaidiousse@gmail.com>
2023-03-27 18:28:50 -0700

    com

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,14 +6,16 @@
  *
  * Return: pointer to destination buffer (dest)
  */
+
 char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	while (*(src + i) != 0)
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		*(dest + i) = *(src + i);
-		i++;
+		dest[i] = src[i];
 	}
+	dest[i] = '\0';
+
 	return (dest);
 }
