ba979792d2197be614fb9dfe438c90de23e734ad
hafsaidiousse <hafsaidiousse@gmail.com>
2023-03-27 17:49:25 -0700

    commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,14 +14,14 @@ void puts_half(char *str)
 	}
 	if (i % 2 == 0)
 	{
-		for (j = i / 2; j < i; j++)
+		for (j = 1 + (i - 1) / 2; j < i; j++)
 		{
 			_putchar(*(str + j));
 		}
 	}
 	else
 	{
-		for (j = (i - 2) / 2; j < i; j++)
+		for (j = 1 + (i - 1) / 2; j < i; j++)
 		{
 			_putchar(*(str + j));
 		}
