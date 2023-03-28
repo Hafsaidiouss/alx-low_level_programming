0ba76f76e25197f16b65074ac92b94b73260714e
hafsaidiousse <hafsaidiousse@gmail.com>
2023-03-27 18:22:35 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcpy - Copier une chaîne
+ * @dest: destination buffer
+ * @src: source buffer
+ *
+ * Return: pointer to destination buffer (dest)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	while (*(src + i) != 0)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = *(src + i);
+	return (dest);
+}
