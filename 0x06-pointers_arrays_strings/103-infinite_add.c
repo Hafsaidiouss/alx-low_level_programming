6c87d27cec700219bf848afbf9597db633620c64
hafsaidiousse <hafsaidiousse@gmail.com>
2023-03-30 17:53:25 -0700

    commit

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,27 +1,89 @@
 #include "main.h"
 /**
+ * rev_string - prints a string, in reverse, followed by a new line.
+ * @s: string
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	char rev;
+	int j;
+
+	while (*(s + i) != 0)
+	{
+		i++;
+	}
+	for (j = 0; j < (i - 1) / 2; j++)
+	{
+		rev = *(s + j);
+		*(s + j) = *(s + i - 1 - j);
+		*(s + i - 1 - j) = rev;
+	}
+	if ((i - 1) % 2 != 0)
+	{
+		rev = *(s + (i - 1) / 2);
+		*(s + (i - 1) / 2) = *(s + (i - 1) / 2 + 1);
+		*(s + (i - 1) / 2 + 1) = rev;
+	}
+}
+/**
  * *infinite_add - adds two numbers.
  * @n1: num 1
  * @n2 numm 2
- * @r: is the buffer that the function will use to store the result
+ * @r: is the buffer that the function will use to store the result
  * @size_r: is the buffer size
  * Return: somme
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	char *k = 0;
-	
-	while ((int)sizeof(r) < size_r && *k <= *n2)
+	int i = 0;
+
+	rev_string(n1);
+	rev_string(n2);
+	while (n1[i] != 0 && n2[i] != 0 && i < size_r)
 	{
-		*r = *n1 + 1;
-		*k = *k + 1;
+		if (i == size_r)
+		{
+			return (0);
+		}
+		if (r[i] + n1[i] +  n2[i] <= 9)
+		{
+			r[i] = r[i] + n1[i] +  n2[i];
+		}
+		else
+		{
+			r[i] = (r[i] + n1[i] +  n2[i]) / 10;
+			r[i + 1] = 1;
+		}
+		i++;
 	}
-	if (*k == *n2)
+	if (n1[i] != 0)
 	{
-		return (r);
+		while (n1[i] != 0)
+		{
+			if (i == size_r)
+			{
+				return (0);
+			}
+			r[i] = n1[i];
+			i++;
+		}
 	}
 	else
 	{
-		return (0);
+		while (n2[i] != 0)
+		{
+			if (i == size_r)
+			{
+				return (0);
+			}
+			r[i] = n2[i];
+			i++;
+		}
 	}
+	rev_string(r);
+	return (r);
+
+
 }
+
