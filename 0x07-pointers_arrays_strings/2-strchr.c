e8b334116a674f544b151f8321d47f3a33eea461
hafsaidiousse <hafsaidiousse@gmail.com>
2023-04-03 15:39:12 -0700

    comit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,14 +8,18 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0;
+	int j = 0;
 
-	while (s[i] != 0)
+	while (s[i])
 	{
-		if (s[i] == c)
-		{
-			return (s + i);
-		}
 		i++;
+	}
+	for (j = 0; j <= i; j++)
+	{
+		if (s[j] == c)
+		{
+			return (s + j);
+		}
 	}
 	return (0);
 }
