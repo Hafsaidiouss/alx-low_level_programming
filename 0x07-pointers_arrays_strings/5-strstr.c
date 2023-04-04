ae5f634565b4d77192570f8fd75660649fede47e
hafsaidiousse <hafsaidiousse@gmail.com>
2023-04-03 17:20:54 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * _strstr -  function that locates a substring
+ * @haystack: char
+ * @needle: char
+ * Return: char
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	while (haystack[i] != 0)
+	{
+		if (haystack[i] == needle[0])
+		{
+			k = i;
+			j = 0;
+			while (needle[j] != 0)
+			{
+				if (haystack[k] == needle[j])
+				{
+					j++;
+					k++;
+				}
+				else
+					break;
+			}
+			if (needle[j] == 0)
+				return (haystack + i);
+		}
+		i++;
+	}
+	return (0);
+}
