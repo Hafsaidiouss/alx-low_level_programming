1f9e8f177fb9a19ccb916101a20f0743aaacb136
hafsaidiousse <hafsaidiousse@gmail.com>
2023-04-05 17:40:52 -0700

    commit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,13 +7,13 @@ void _print_rev_recursion(char *s)
 {
 	if (*s != 0)
 	{
-		if (*s++ == 0)
+		if (*(s + 1) == 0)
 		{
 			_putchar(*s);
 		}
 		else
 		{
-			_print_rev_recursion(s++);
+			_print_rev_recursion((s + 1));
 			_putchar(*s);
 		}
 	}
