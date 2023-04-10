c256104588d6ed1ad6f6fe4094a5eeabd8c86a7f
hafsaidiousse <hafsaidiousse@gmail.com>
2023-04-10 10:57:38 -0700

    commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main fonction
+ * @argc: number of argument
+ * @argv: vector of argument
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	if (argv)
+	{
+	printf("%d\n", argc - 1);
+	}
+	return (0);
+}
