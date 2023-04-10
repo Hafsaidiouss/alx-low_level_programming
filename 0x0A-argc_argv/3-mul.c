7e1f208bec14875026eaf54958364b870a4f51b3
hafsaidiousse <hafsaidiousse@gmail.com>
2023-04-10 11:18:20 -0700

    commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main fonction
+ * @argc: number of argument
+ * @argv: vector of argument
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Error");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
