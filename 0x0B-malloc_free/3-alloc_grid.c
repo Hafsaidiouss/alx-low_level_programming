4aeaed7beeac8a241b54d9cf1913a801d31541a0
hafsaidiousse <hafsaidiousse@gmail.com>
2023-04-11 17:08:16 -0700

    commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,8 +16,14 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	p = malloc(width);
+	if (p == NULL)
+		return (NULL);
 	for (i = 0; i < height; i++)
+	{
 		p[i] = malloc(width);
+		if (p[i] == NULL)
+			return (NULL);
+	}
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			p[i][j] = 0;
