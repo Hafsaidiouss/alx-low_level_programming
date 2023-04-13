43c01c8b094645e69fe482aa41e1e04a61067ede
hafsaidiousse <hafsaidiousse@gmail.com>
2023-04-13 15:13:18 -0700

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < size1; i++)
 		dest[i] = s1[i];
 	for (i = 0; i < size2 && i < (int) n; i++)
-		if (s2 != NULL)
 			dest[i + size1] = s2[i];
 	dest[i + size1] = 0;
 	return (dest);
