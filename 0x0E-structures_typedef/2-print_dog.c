30c98c7d31113a0144817288fbd88ad7efde77f0
hafsaidiousse <hafsaidiousse@gmail.com>
2023-04-17 17:39:06 -0700

    commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,14 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name != NULL)
 		printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
+	else
+		printf("Name: (nil)");
+	if (age >= 0)
+		printf("Age: %f\n", d->age);
+	else
+		printf("Age: (nil)");
 	if (d->owner != NULL)
 		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: (nil)");
 }
