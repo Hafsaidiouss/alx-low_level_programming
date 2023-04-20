6de5cef4ca5994846d59683eb2c1d8ad391b2571
hafsaidiousse <hafsaidiousse@gmail.com>
2023-04-19 18:16:15 -0700

    commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,8 +18,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 			if (cmp(array[i]))
 				return (i);
 		}
-		if (size < 0)
-			return (-1);
-		return (-1);
 	}
+	if (size < 0)
+		return (-1);
+	return (-1);
+	
 }
