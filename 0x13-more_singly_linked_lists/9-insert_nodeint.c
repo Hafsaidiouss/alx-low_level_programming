78920515d561156b760186cc2e219e9353dfefa7
hafsaidiousse <hafsaidiousse@gmail.com>
2023-05-01 11:49:47 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (nv == NULL)
 		return (NULL);
-	while (temp != NULL && i <= idx)
+	while (temp != NULL && (i + 1) <= idx)
 	{
 		if ((i + 1) == idx)
 		{
